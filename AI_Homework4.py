f3d25ff3b08fc783cad92e16a934dffd073da81a
nguye404 <34149502+nguye404@users.noreply.github.com>
2018-05-31 12:34:33 -0700

    fix rand to work on float number

--- AI_Homework4.py
+++ AI_Homework4.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 import numpy as np
 
-def processFile(filename):
+def processFile(filename, k):
     inFile = open(filename, "r")
 
     D = OrderedDict()
@@ -23,9 +23,9 @@ def processFile(filename):
         # get replaced when it has same compactness
         D[i] = (float(curList[2]), float(curList[3]), float(curList[4]))
 
-        listCompactness = curList[2]
-        listLength = curList[3]
-        listWidth = curList[4]
+        listCompactness.append(curList[2])
+        listLength.append(curList[3])
+        listWidth.append(curList[4])
         #print(D[i])
         i = i + 1
         
@@ -33,17 +33,24 @@ def processFile(filename):
         currentLine = inFile.readline()
 
     #X = np.array(list(zip(range(listCompactness), listLength, listWidth)))
-    # Number of clusters
-    k = 3
+    print(listCompactness)
+    
+    maxCompactness = float(max(listCompactness))
+    maxLength = float(max(listLength))
+    maxWidth = float(max(listWidth))
+    print(max(listCompactness), maxLength, maxWidth)
+    
     # X coordinates of random centroids
-    C_x = np.random.randint(0, max(listCompactness), size=k)
+    C_x = np.random.uniform(0.0, maxCompactness, size=k)
     # Y coordinates of random centroids
-    C_y = np.random.randint(0, max(listLength), size=k)
-    C_z = np.random.randint(0, max(listWidth), size=k)
-    C = np.array(list(zip(C_x, C_y, C_z)), dtype=np.float32)
+    C_y = np.random.uniform(0, maxLength, size=k)
+    # Z coordinates of random centroids
+    C_z = np.random.uniform(0, maxWidth, size=k)
+    
+    C = np.array(list(zip(C_x, C_y, C_z)), dtype=np.float)
     print(C)
 
-processFile("seeds_dataset.txt")
+processFile("seeds_dataset.txt", 3)
 
 
 
